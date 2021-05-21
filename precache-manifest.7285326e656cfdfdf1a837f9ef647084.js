787dd9c067f9c6b1a32b5c87757103cd6fb56a6a
asdf <safaasadi21@yahoo.com>
2021-05-21 12:49:28 -0400

    Updates

---
+++ precache-manifest.7285326e656cfdfdf1a837f9ef647084.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42a58aac7603f318f795",
+    "url": "/me/static/css/main.3e32d842.chunk.css"
+  },
+  {
+    "revision": "42a58aac7603f318f795",
+    "url": "/me/static/js/main.42a58aac.chunk.js"
+  },
+  {
+    "revision": "1caf0789ee8fdb1f0b3c",
+    "url": "/me/static/js/runtime~main.1caf0789.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/me/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "804eff71f06d8759c1a1c5069834a41c",
+    "url": "/me/index.html"
+  }
+];
